470dac824a30807df650dca892c57c51ca3ef604
Marien Fressinaud <dev@marienfressinaud.fr>
2015-02-10 18:13:48 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Pandoc Generator
+
+A Pelican plugin to use Pandoc software.
+
+Pandoc is a powerful tool to transform text from different formats (e.g. ReST,
+Markdown) to others. It is really helpful to generate PDF or EPUB. It also can easily replace the "official" PDF plugin.
+
+This plugin provides a powerful interface to handle document generation through
+Pandoc.
+
+Markdown parsing is done in MultiMarkdown to be able to parse metadata information easily.
+
+## Installation
+
+You need to have [Pandoc](http://johnmacfarlane.net/pandoc/) (!) and [pypandoc](https://github.com/bebraw/pypandoc), a Python module wrapper for Pandoc, installed on your system. You can install pypandoc with `pip`:
+
+```bash
+$ pip install pypandoc
+```
+
+Then, put the plugin directory in your plugin directory. For instance, create a `./plugins` directory in your Pelican project and add these lines in your `pelicanconf.py` file:
+
+```python
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['pandoc']
+```
+
+Obviously, you'll have to adapt instructions if you already have installed some plugins.
+
+To use this plugin then, you'll need to set a `PANDOC_OUTPUTS` configuration variable. It is a dictionary where the keys are the output formats and the
+values are the corresponding destination directories. For instance:
+
+```python
+PANDOC_OUTPUTS = {
+    'pdf': 'pdfs',
+    'epub': 'epubs',
+}
+```
+
+Note you'll have to modify your theme template to support download of files. Here is a snippet to download files (PDF or EPUB) of a specific article. You can put it in the `templates/article_infos.html` file (or similar) of your theme:
+
+```jinja
+{% if 'pdf' in PANDOC_OUTPUTS or 'epub' in PANDOC_OUTPUTS %}
+    <div class="entry-download">
+        {% if 'pdf' in PANDOC_OUTPUTS %}
+            <a href="{{ SITEURL }}/{{ PANDOC_OUTPUTS['pdf'] }}/{{ article.slug }}.pdf">Download as PDF</a><br />
+        {% endif %}
+        {% if 'epub' in PANDOC_OUTPUTS %}
+            <a href="{{ SITEURL }}/{{ PANDOC_OUTPUTS['epub'] }}/{{ article.slug }}.epub">Download as EPUB</a>
+        {% endif %}
+    </div>
+{% endif %}
+```
+
+## How-to contribute
+
+- Fork the [project Git repository](https://github.com/marienfressinaud/Pelidoc) ;
+- Please open a ticket [on the bug tracker](https://github.com/marienfressinaud/Pelidoc/issues) so we can discuss about your bug / feature ;
+- Create a dedicated branch for your fix (`git checkout -b` is your friend) ;
+- Commit your work, push your branch upstream (`git push --set-upstream origin your_branch_name`) and do a [pull request](https://github.com/marienfressinaud/Pelidoc/compare).
+
+Note there's a bunch of TODOs in the source code that you may fix.
+
+## Credits
+
+- [Pelican](http://getpelican.com) by [Alexis Métaireau](http://blog.notmyidea.org/) ;
+- [Pelican plugins](https://github.com/getpelican/pelican-plugins) for the code basis (especially PDF plugin!)
+- [pypandoc](https://github.com/bebraw/pypandoc) by [Juho Vepsäläinen](http://www.nixtu.info/) ;
+- [Pandoc Reader](https://github.com/liob/pandoc_reader), another Pandoc plugin for Pelican but a bit less powerful, by [Hinrich B. Winther](https://github.com/liob).
+- And, obviously, [Pandoc](http://johnmacfarlane.net/pandoc/) by [John MacFarlane](http://johnmacfarlane.net).
---
+++ pandoc.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""A Pelican plugin to use Pandoc software.
+
+Pandoc is a powerful tool to transform text from different formats (e.g. ReST,
+Markdown) to others. It is really helpful to generate PDF or EPUB.
+
+This plugin provides a powerful interface to handle document generation through
+Pandoc.
+
+Here is the list of configuration variables.
+
+PANDOC_OUTPUTS          a dict where the keys are the output formats and the
+                        values are the destination directories. Note if
+                        to_format is set at "pdf", the "to"-argument is set at
+                        latex" for Pandoc which does not support "-t pdf"
+                        argument.
+PANDOC_EXPORT_ARTICLES  True (default) if you want to export articles, False
+                        else.
+PANDOC_EXPORT_PAGES     True (default) if you want to export pages, False else.
+
+Important note: this plugin requires Pandoc and pypandoc to be installed on
+your system to work!
+
+"""
+
+from __future__ import unicode_literals
+
+import os
+import logging
+
+from pelican import signals
+from pelican.generators import Generator
+
+# TODO: it could be great to not depend on an external module for such a basic
+# work.
+import pypandoc
+
+
+logger = logging.getLogger(__name__)
+
+
+class PandocGenerator(Generator):
+    """The Pandoc generator.
+
+    It takes the list of articles / pages and generates files according to
+    PANDOC_OUTPUTS configuration variable.
+
+    """
+
+    def guess_format(self, content):
+        """Return the format used by a given content.
+
+        Since Pelican only supports reStructuredText and MarkDown files, this
+        method returns just "md" or "rst".
+
+        It is based on the file extension.
+
+        TODO: it could be great if Pelican could support this method itself.
+
+        :param content: the content (article or page)
+        :type content: pelican.contents.Content
+        :return: the article type (rst or md)
+        :rtype: str
+        :raises: KeyError if the file extension is not supported
+
+        """
+        formats = {
+            '.rst': 'rst',
+            '.md': 'md',
+            '.markdown': 'md',
+            '.mkd': 'md',
+            '.mdown': 'md',
+        }
+
+        file_name, file_extension = os.path.splitext(content.source_path)
+
+        return formats[file_extension]
+
+    def generate_files(self, content):
+        """Generates the list of files for a given content.
+
+        :param content: the content to generate.
+        :type content: pelican.contents.Content
+
+        """
+        try:
+            from_format = self.guess_format(content)
+        except KeyError:
+            # The content format is not supported.
+            return
+
+        list_outputs = self.settings.get('PANDOC_OUTPUTS', {})
+        for to_format, output_dir in list_outputs.items():
+            output_dir = os.path.join(self.output_path, output_dir)
+
+            # Create the output directory if it does not exist yet.
+            # TODO: this big-block is quite ugly, please move it elsewhere.
+            if not os.path.isdir(output_dir):
+                try:
+                    os.mkdir(output_dir)
+                except OSError:
+                    logger.error(
+                        "Couldn't create the {format} output folder in {dir}".format(
+                            format=to_format,
+                            dir=output_dir
+                        )
+                    )
+
+            filename = "{id_file}.{extension}".format(
+                id_file=content.slug,
+                extension=to_format
+            )
+            filepath = os.path.join(output_dir, filename)
+
+            if to_format == 'pdf':
+                # Pandoc don't take "pdf" as an output value.
+                # Use latex instead.
+                to_format = 'latex'
+
+            if from_format == 'md':
+                # Use the same format as Pelican (paticularly for metadata!)
+                from_format = 'markdown_mmd'
+
+            # Here is the magic!
+            # TODO: support extra_args extending (it could be useful to use
+            # specific Pandoc template).
+            pypandoc.convert(
+                source=content.source_path,
+                to=to_format,
+                format=from_format,
+                extra_args=(
+                    '--smart',
+                    '--standalone',
+                    '-o', filepath,
+                )
+            )
+
+            logger.info("[ok] writing {filepath}".format(
+                filepath=filepath
+            ))
+
+    def generate_output(self, writer=None):
+        """Generate files for each articles and pages.
+
+        If PANDOC_EXPORT_ARTICLES is False, articles are not generated.
+        If PANDOC_EXPORT_PAGES is False, pages are not generated.
+
+        We don't use the writer passed as argument since we write our own
+        files (© PDF plugin :)).
+
+        """
+        contents_to_export = []
+        if self.settings.get('PANDOC_EXPORT_ARTICLES', True):
+            contents_to_export += self.context['articles']
+
+        if self.settings.get('PANDOC_EXPORT_PAGES', True):
+            contents_to_export += self.context['pages']
+
+        for content_to_export in contents_to_export:
+            self.generate_files(content_to_export)
+
+
+def get_generators(generators):
+    return PandocGenerator
+
+
+def register():
+    """Register our Pandoc class to the Pelican generators."""
+    signals.get_generators.connect(get_generators)
